cb6f75c177fada2e1c71db568c743534aa8e11a0
sharlisbg <sharislizza15@gmail.com>
2023-02-24 08:04:21 -0600

    Creación de clases base

---
+++ IStack.java
@@ -0,0 +1,11 @@
+public interface IStack<T> {
+    int count();
+	
+	boolean isEmpty();
+	
+	void push(T value);
+	
+	T pull();
+	
+	T peek();
+}
---
+++ Stack.java
@@ -0,0 +1,33 @@
+public class Stack<T> implements IStack<T> {
+
+    @Override
+    public int count() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public void push(T value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    @Override
+    public T pull() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pull'");
+    }
+
+    @Override
+    public T peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+    
+}
